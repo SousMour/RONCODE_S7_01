a4f654b45740f84d2087a4baf4650bed2b019fd6
SousMour <142849312+SousMour@users.noreply.github.com>
2023-09-20 19:15:28 -0300

    Add files via upload

---
+++ impar_.py
@@ -0,0 +1,7 @@
+def impar(n):
+    return n%2 != 0
+
+numero=int(input())
+valor=impar(numero)
+print(valor)
+
---
+++ media.py
@@ -0,0 +1,13 @@
+nota_1=int(input())
+nota_2=int(input())
+nota_3=int(input())
+media=(nota_1 + nota_2+ nota_3)/3
+
+if nota_3 > 8:
+   final= media + 1
+
+final > 10:
+    round(final)
+print(final)
+    
+
